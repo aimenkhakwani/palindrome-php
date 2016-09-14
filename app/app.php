c7ca2f432bc6c643e6da64bc4433a1399eb4336b
Aimen Khakwani and Mark Lawson <mark@>
2016-09-14 14:34:08 -0700

    Add style to the templates

--- app/app.php
+++ app/app.php
@@ -2,15 +2,24 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Palindrome.php";
 
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
     $app = new Silex\Application();
+    $app['debug'] = true;
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__.'/../views'
+        'twig.path' => __DIR__.'/../view'
     ));
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('home.html.twig');
     });
 
+    $app->get("/output", function() use ($app) {
+        $newPali = new Palindrome;
+        $output = $newPali->evaluate($_GET['input']);
+        return $app['twig']->render('output.html.twig', array('output'=>$output));
+    });
 
     return $app;
 ?>
